3788e58cfe40dc5c1d33e430e1c36ea2fdaa621f
crumpetalpaca <crumpet@macaw.me>
2025-03-08 13:54:56 +0000

    test actions

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 cd android
-echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks
-echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
-echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
+echo $KEY_STORE | base64 --decode --ignore-garbage > key.jks
+echo "storePassword=${KEY_PASS}" >> key.properties
+echo "keyPassword=${KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
 echo "storeFile=../key.jks" >> key.properties
-echo $PLAYSTORE_DEPLOY_KEY >> keys.json
 ls | grep key
 bundle install
 bundle update fastlane
