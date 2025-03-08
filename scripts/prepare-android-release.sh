6b14b8f178d69181c7ea50e84e3a038cd1ad6bec
crumpetalpaca <crumpet@macaw.me>
2025-03-08 14:35:54 +0000

    fix keystore

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -3,7 +3,7 @@ cd android
 echo $KEY_STORE | base64 --decode --ignore-garbage > key.jks
 echo "storePassword=${KEY_PASS}" >> key.properties
 echo "keyPassword=${KEY_PASS}" >> key.properties
-echo "keyAlias=key" >> key.properties
+echo "keyAlias=crumpet" >> key.properties
 echo "storeFile=../key.jks" >> key.properties
 ls | grep key
 bundle install
